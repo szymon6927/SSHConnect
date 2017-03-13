036ba99b17f8e6d831dcbf2fe79a2e6aa3c68a8c
szymon6927 <miks.szymon@gmail.com>
2017-03-13 20:44:11 +0100

    Add script to repository

---
+++ sshConnect.py
@@ -0,0 +1,62 @@
+import threading, paramiko, sys
+ 
+class ssh:
+    shell = None
+    client = None
+    transport = None
+ 
+    def __init__(self, address, username, password):
+        print("Łączenie z serwerem", str(address) + ".")
+        self.client = paramiko.client.SSHClient()
+        self.client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
+        self.client.connect(address, username=username, password=password, look_for_keys=False)
+        self.transport = paramiko.Transport((address, 22))
+        self.transport.connect(username=username, password=password)
+ 
+        thread = threading.Thread(target=self.process)
+        thread.daemon = True
+        thread.start()
+ 
+    def closeConnection(self):
+        if(self.client != None):
+            self.client.close()
+            self.transport.close()
+ 
+    def openShell(self):
+        self.shell = self.client.invoke_shell()
+ 
+    def sendShell(self, command):
+        if(self.shell):
+            self.shell.send(command + "\n")
+        else:
+            print("Sesja nie otwarta.")
+ 
+    def process(self):
+        global connection
+        while True:
+            # Print data when available
+            if self.shell != None and self.shell.recv_ready():
+                alldata = self.shell.recv(1024)
+                while self.shell.recv_ready():
+                    alldata += self.shell.recv(1024)
+                strdata = str(alldata, "utf8")
+                strdata.replace('\r', '')
+                print(strdata, end = "")
+                if(strdata.endswith("$ ")):
+                    print("\n$ ", end = "")
+ 
+ 
+sshServer = input("SSH server: ")
+sshUsername = input("SSH username: ")
+sshPassword = input("SSH password: ")
+ 
+ 
+connection = ssh(sshServer, sshUsername, sshPassword)
+connection.openShell()
+while True:
+    command = input('$ ')
+    if command.startswith(" "):
+        command = command[1:]
+    connection.sendShell(command)
+    if command == "q":
+        sys.exit()
